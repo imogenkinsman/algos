f5a5e2380e4a39771f7fa67b36ee313ae2f0ba5d
Jade McGough <jade@thezets.com>
2014-01-06 07:46:39 -0800

    max subarray

---
+++ spec/max_subarray_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../lib/max_subarray'
+
+describe 'Max Subarray' do
+
+  it "determines max subarray from an array with odd elements" do
+    ary = [5, 6, -2, 3, 24, -1, -2, 6, -7, 13, -22, 5, 7, 1]
+    expect(max_subarray(ary)).to eq [0, 9, 45]
+  end
+
+  it "determines max subarray from an array with even elements" do
+    ary = [5, 6, -2, 3, 24, -1, -2, 6, -7, 13, -22, 5, 7]
+    expect(max_subarray(ary)).to eq [0, 9, 45]
+  end
+
+  it "works when the max subarray is in the middle" do
+    ary = [-3, 5, -1, 5, 3, 2, -20, -12]
+    expect(max_subarray(ary)).to eq [1, 5, 14]
+  end
+
+  it "works when the max subarray is in the first half" do
+    ary = [1, 3, 12, -10, 3, -2, 1, 0]
+    expect(max_subarray(ary)).to eq [0, 2, 16]
+  end
+
+  it "works when the max subarray is in the second half" do
+    ary = [1, 2, 3, 4, -30, 10, 3, 5, -2]
+    expect(max_subarray(ary)).to eq [5, 7, 18]
+  end
+
+end
