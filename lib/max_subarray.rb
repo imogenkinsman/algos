77bfed80f4a655a0b3ef0404140206cf85826508
Jade McGough <jade@thezets.com>
2014-01-06 13:03:40 -0800

    kadane's algorithm

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -30,13 +30,15 @@ end
 
 def max_subarray(ary, low = 0, high = ary.size - 1)
   return [low, high, ary[low]] if high == low
+
   mid = (low + high) / 2
   left_low, left_high, left_sum = max_subarray(ary, low, mid)
   right_low, right_high, right_sum = max_subarray(ary, mid + 1, high)
   cross_low, cross_high, cross_sum = max_crossing_subarray(ary, low, mid, high)
-  if left_sum >= right_sum && left_sum >= cross_sum
+  case [left_sum, right_sum, cross_sum].max
+  when left_sum
     return [left_low, left_high, left_sum]
-  elsif right_sum >= left_sum && right_sum >= cross_sum
+  when right_sum
     return [right_low, right_high, right_sum]
   else
     return [cross_low, cross_high, cross_sum]
