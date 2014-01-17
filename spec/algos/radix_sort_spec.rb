6f513e6682f478092803a182004d9467f40f6a0c
Jade McGough <jade@thezets.com>
2014-01-17 02:03:05 -0800

    added second radix sort test

--- spec/algos/radix_sort_spec.rb
+++ spec/algos/radix_sort_spec.rb
@@ -7,4 +7,9 @@ describe 'radix sort' do
     expect(radix_sort(array)).to eq [0,0,1,1,3,6,7,8,8,9]
   end
 
+  it 'sorts an array with multiple digits' do
+    array = [2,1,34,21,5,3,77,2,45,102,23,21]
+    expect(radix_sort(array)).to eq [1, 2, 2, 3, 5, 21, 21, 23, 34, 45, 77, 102]
+  end
+
 end
